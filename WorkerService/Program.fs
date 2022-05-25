a59f98d2d19793bdf25e214738e0d25c22e2c18a
Ricardo Milan <ricardo@fluencypass.com>
2022-05-25 09:36:25 -0300

    Add project files.

---
+++ WorkerService/Program.fs
@@ -0,0 +1,18 @@
+﻿module workerservice.App
+
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+
+open workerservice.NumberService
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Iniciando Worker!"
+
+    let host = 
+        Host.CreateDefaultBuilder(argv)
+            .ConfigureServices(fun hostContext services ->
+                services.AddHostedService<WorkerSample1>() |> ignore
+            ).Build().Run()
+
+    0
---
+++ WorkerService/Services/WorkerSample1.fs
@@ -0,0 +1,23 @@
+﻿namespace workerservice
+
+module NumberService = 
+
+    open System.Threading
+    open System.Threading.Tasks
+    open Microsoft.Extensions.Configuration
+    open Microsoft.Extensions.Hosting
+    open Microsoft.Extensions.Logging
+    
+    type WorkerSample1(logger : ILogger<WorkerSample1>, configuration : IConfiguration ) =
+        inherit BackgroundService()
+
+        override _.ExecuteAsync(stoppingToken : CancellationToken) =
+            task {
+                // let printNums = (fun x ->  printf "%A\n" x)
+                
+                // [ for i in 1 .. 100 do yield printNums i ] |> ignore
+                
+                let printNums = (fun x ->  logger.Log(LogLevel.Information, "{0}", (int)x)   )
+
+                [1..100] |> List.map printNums |> ignore
+            } :> Task
